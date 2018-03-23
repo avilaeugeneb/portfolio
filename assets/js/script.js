2ddf467da69ba51d8d6af84afe81edffad5a4a4b
avilaeugeneb <avila.eugeneb@gmail.com>
2018-03-23 15:36:25 +0800

    Added sticky nav and progress bar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,11 @@
 // console.log('Hello world! I am ready for Full-stack Web Development.');
+
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("my-bar").style.width = scrolled + "%";
+}
